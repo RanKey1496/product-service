6b7f1e777f53c57453c833b0ef121465b73946ab
Santiago Umania <sumana@vertical.com.co>
2018-10-20 11:09:21 -0500

    fixed server.js error

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import App from './app';
+
+console.info(`
+Products Service
+`);
+
+process.on('uncaughtException', (err) => {
+    console.error(`
+    --------------------
+    Unhandled Exception:
+    ${err.message}
+    --------------------
+    `);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.error(`
+    --------------------
+    Unhandled Rejection:
+    ${err.message}
+    --------------------
+    `);
+});
+
+const app: App = new App();
+app.start();
+module.exports = app;
